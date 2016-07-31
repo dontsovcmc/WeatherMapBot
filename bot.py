4e2e60a8f962bb47dd3741ee5f327c332e501473
doncov.eugene <doncov.eugene@alphaopen.com>
2016-07-31 17:49:17 +0400

    test update

--- bot.py
+++ bot.py
@@ -6,18 +6,15 @@ from logger import log
 from datetime import datetime, timedelta
 
 from telegram.ext import Updater
-from telegram.ext import CommandHandler, InlineQueryHandler, CallbackQueryHandler
+from telegram.ext import CommandHandler, RegexHandler
 from telegram.ext import MessageHandler, Filters, ConversationHandler
-from telegram import ForceReply, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
-from telegram.inlinequeryresultlocation import InlineQueryResultLocation
+from telegram import ReplyKeyboardMarkup, KeyboardButton
 
 from sqlalchemy.orm.exc import NoResultFound
 
 from service import exception_info
 
 from core import get_continent_name_id, get_region_name_id, get_type_name_id
-from init import init
-from core import maps_keyboard_layout, continent_keyboard_layout, region_keyboard_layout
 from core import get_map_id, get_map_type_id, get_region_id, get_continent_id
 from core import get_map, get_continent_name, get_region_name, get_map_type_name
 from core import get_legend, get_previous_timestamp_by_path, get_next_timestamp_by_path
@@ -80,13 +77,14 @@ def location(bot, update):
 
 def select_continent(bot, update):
 
+    chat_id = update.message.chat_id
     text = update.message.text
 
     if text == '/manual' or text == u'Выбрать карту':
 
         reply_keyboard = [[KeyboardButton(d[0])] for d in get_continent_name_id()]
 
-        bot.sendMessage(update.message.chat_id,
+        bot.sendMessage(chat_id,
                     text=u'Выберите континент',
                     reply_markup=ReplyKeyboardMarkup(reply_keyboard), one_time_keyboard=True)
         return STATE_REGION
@@ -95,38 +93,40 @@ def select_continent(bot, update):
 
 
 def continent_handler(bot, update):
+    chat_id = update.message.chat_id
     text = update.message.text
 
     found = [cont for cont in get_continent_name_id() if cont[0] == text]
 
     if found:
         with Shelve() as sh:
-            sh.set(update.message.chat_id, CONTINENT, get_continent_id(text))
+            sh.set(chat_id, CONTINENT, get_continent_id(text))
 
         reply_keyboard = [[KeyboardButton(d[0])] for d in get_region_name_id()]
 
-        bot.sendMessage(update.message.chat_id,
+        bot.sendMessage(chat_id,
                     text=u'Выберите регион',
                     reply_markup=ReplyKeyboardMarkup(reply_keyboard), one_time_keyboard=True)
-        return STATE_TYPE
+        return STATE_REGION
 
     return STATE_CONT
 
 
 def region_handler(bot, update):
+    chat_id = update.message.chat_id
     text = update.message.text
 
     found = [cont for cont in get_region_name_id() if cont[0] == text]
     if found:
         with Shelve() as sh:
-            sh.set(update.message.chat_id, REGION, get_region_id(text))
+            sh.set(chat_id, REGION, get_region_id(text))
 
         reply_keyboard = [[KeyboardButton(d[0])] for d in get_type_name_id()]
 
-        bot.sendMessage(update.message.chat_id,
+        bot.sendMessage(chat_id,
                     text=u'Выберите тип',
                     reply_markup=ReplyKeyboardMarkup(reply_keyboard), one_time_keyboard=True)
-        return STATE_MAP
+        return STATE_TYPE
 
     return STATE_REGION
 
@@ -142,18 +142,26 @@ def type_handler(bot, update):
 
         with Shelve() as sh:
             sh.set(chat_id, MAPTYPE, type_id)
+
             map_id = get_map_id(sh.get(chat_id, REGION), type_id)
             sh.set(chat_id, MAPID, map_id)
 
         return send_map(bot, map_id, timestamp)
 
     except Exception, err:
-        log.error('type_handler error: %s' % str(err))
-        log.error(exception_info())
-        report.track_screen(user_id, '/error')
         bot.sendMessage(chat_id, text=u"Что-то пошло не так. Начать сначала: /start")
 
     return STATE_REGION
+
+
+def refresh_handle(bot, update):
+    chat_id = update.message.chat_id
+    timestamp = datetime.now()
+
+    with Shelve() as sh:
+        map_id = sh.get(chat_id, MAPID)
+        return send_map(bot, map_id, timestamp)
+
 
 def send_map(bot, update, map_id, timestamp):
 
@@ -196,185 +204,51 @@ def timestamp_handler(bot, update):
     user_id = update.message.from_user.id
 
 
-
-
 def cancel_handler(bot, update):
     return STATE_MENU
 
 
-def start1(bot, update):
+def previous_handler(bot, update):
+
     chat_id = update.message.chat_id
     user_id = update.message.from_user.id
-    text = update.message.text
 
-    shelve_db.open()
+    report.track_screen(user_id, PREVIOUS_MAP)
 
     try:
-        chat_user = shelve_db.get(chat_id, USER)
+        with Shelve() as sh:
+            p = sh.get(chat_id, 'last_map')
+            map_id = sh.get(chat_id, MAPID)
 
-        if text == '/start' or not chat_user:
-            report.track_screen(user_id, '/start')
-            shelve_db.set(chat_id, USER, user_id)
-            shelve_db.set(chat_id, CONTINENT, None)
-            shelve_db.set(chat_id, REGION, None)
-            shelve_db.set(chat_id, MAPTYPE, None)
+        timestamp = get_previous_timestamp_by_path(p)
 
-            start_reply_markup = ReplyKeyboardMarkup(continent_keyboard_layout(KeyboardButton),
-                                                     one_time_keyboard=True, resize_keyboard=True)
+        send_map(bot, update, map_id, timestamp)
 
-            bot.sendMessage(chat_id, text=CHANGE_CONT, reply_markup=start_reply_markup)
+    except NoResultFound:
+        report.track_screen(user_id, u'<изображение отсутствует>')
+        bot.sendMessage(chat_id, text=u'\n<изображение отсутствует>')
 
-        elif chat_user and chat_user == user_id:
 
-            if text == CHANGE_CONT:
-                report.track_screen(user_id, CHANGE_CONT)
-                shelve_db.set(chat_id, CONTINENT, None)
-                shelve_db.set(chat_id, REGION, None)
-                shelve_db.set(chat_id, MAPTYPE, None)
-                update.message.text = '/start'
-                start(bot, update)
+def next_handler(bot, update):
 
-            elif text == CHANGE_REGION:
-                report.track_screen(user_id, CHANGE_REGION)
-                update.message.text = get_continent_name(shelve_db.get(chat_id, CONTINENT, ''))
-                shelve_db.set(chat_id, CONTINENT, None)
-                shelve_db.set(chat_id, REGION, None)
-                shelve_db.set(chat_id, MAPTYPE, None)
-                start(bot, update)
+    chat_id = update.message.chat_id
+    user_id = update.message.from_user.id
 
-            elif text == CHANGE_MAP:
-                report.track_screen(user_id, CHANGE_MAP)
-                update.message.text = get_region_name(shelve_db.get(chat_id, REGION, ''))
-                shelve_db.set(chat_id, REGION, None)
-                shelve_db.set(chat_id, MAPTYPE, None)
+    report.track_screen(user_id, PREVIOUS_MAP)
 
-                start(bot, update)
+    try:
+        with Shelve() as sh:
+            p = sh.get(chat_id, 'last_map')
+            map_id = sh.get(chat_id, MAPID)
 
-            elif text == REFRESH:
-                report.track_screen(user_id, REFRESH)
-                id = shelve_db.get(chat_id, MAPTYPE)
-                if id:
-                    update.message.text = get_map_type_name(id)
-                else:
-                    id = shelve_db.get(chat_id, REGION)
-                    if id:
-                        update.message.text = get_region_name(id)
-                    else:
-                        id = shelve_db.get(chat_id, CONTINENT)
-                        if id:
-                            update.message.text = get_continent_name(id)
-                        else:
-                            update.message.text = '/start'
+        timestamp = get_next_timestamp_by_path(p)
 
-                start(bot, update)
+        send_map(bot, update, map_id, timestamp)
 
-            elif text == PREVIOUS_MAP:
-                report.track_screen(user_id, PREVIOUS_MAP)
+    except NoResultFound:
+        report.track_screen(user_id, u'<изображение отсутствует>')
+        bot.sendMessage(chat_id, text=u'\n<изображение отсутствует>')
 
-                p = shelve_db.get(chat_id, 'last_map')
-                if not p:
-                    update.message.text = REFRESH
-                    start(bot, update)
-
-                try:
-                    timestamp = get_previous_timestamp_by_path(p)
-                    name = get_map_type_name(shelve_db.get(chat_id, MAPTYPE))
-                    update.message.text = name + timestamp.strftime(' (%d.%m.%Y %H:%M)')
-                    start(bot, update)
-                except NoResultFound:
-                    report.track_screen(user_id, u'<изображение отсутствует>')
-                    bot.sendMessage(chat_id, text=u'\n<изображение отсутствует>', reply_markup=show_map_reply_markup)
-
-            elif text == NEXT_MAP:
-                report.track_screen(user_id, NEXT_MAP)
-
-                p = shelve_db.get(chat_id, 'last_map')
-                if not p:
-                    update.message.text = REFRESH
-                    start(bot, update)
-
-                try:
-                    timestamp = get_next_timestamp_by_path(p)
-                    name = get_map_type_name(shelve_db.get(chat_id, MAPTYPE))
-                    update.message.text = name + timestamp.strftime(' (%d.%m.%Y %H:%M)')
-                    start(bot, update)
-
-                except NoResultFound:
-                    report.track_screen(user_id, u'no_image')
-                    bot.sendMessage(chat_id, text=u'\n<изображение отсутствует>', reply_markup=show_map_reply_markup)
-
-            else:
-                map_type_id = shelve_db.get(chat_id, MAPTYPE)
-                region_id = shelve_db.get(chat_id, REGION)
-                continent_id = shelve_db.get(chat_id, CONTINENT)
-
-                if not continent_id:
-
-                    cont_id = get_continent_id(text)
-                    shelve_db.set(chat_id, CONTINENT, cont_id)
-
-                    reply_markup = ReplyKeyboardMarkup(region_keyboard_layout(cont_id, KeyboardButton),
-                                                       one_time_keyboard=True, resize_keyboard=True)
-                    bot.sendMessage(chat_id, text=CHANGE_REGION, reply_markup=reply_markup)
-
-                    report.track_screen(user_id, '/%s' % (continent_id))
-
-                elif not region_id:
-
-                    region_id = get_region_id(text)
-                    shelve_db.set(chat_id, REGION, region_id)
-
-                    reply_markup = ReplyKeyboardMarkup(maps_keyboard_layout(region_id, KeyboardButton),
-                                                       one_time_keyboard=True, resize_keyboard=True)
-                    bot.sendMessage(chat_id, text=CHANGE_MAP, reply_markup=reply_markup)
-
-                    report.track_screen(user_id, '/%s/%s' % (continent_id, region_id))
-
-                else: # not map_type_id
-
-                    if len(update.message.text.split(' (')) != 2:
-                        map_type_id = get_map_type_id(update.message.text)
-                        timestamp = datetime.now()
-                    else:
-                        map_type_name, timestr = update.message.text.split(' (')
-                        map_type_id = get_map_type_id(map_type_name)
-                        timestamp = datetime.strptime(timestr, '%d.%m.%Y %H:%M)') - timedelta(seconds=1)
-
-                    report.track_screen(user_id, '/%s/%s/%s' % (continent_id, region_id, map_type_id))
-
-                    shelve_db.set(chat_id, MAPTYPE, map_type_id)
-                    map_id = get_map_id(region_id, map_type_id)
-
-                    path, info = get_map(map_id, timestamp)
-                    log.info('path %s' % path)
-
-                    p = shelve_db.get(chat_id, 'last_map')
-                    if p and p == path:
-                        bot.sendMessage(chat_id, text=u'<обновление отсутствует>', reply_markup=show_map_reply_markup)
-
-                        report.track_screen(user_id, '/%s/%s/%s/%s/no_update' % (
-                        continent_id, region_id, map_type_id, map_id))
-                    else:
-
-                        if path:
-                            with open(path.encode('utf-8'), 'rb') as image:
-                                bot.sendPhoto(chat_id, image, caption=info.encode('utf-8'), reply_markup=show_map_reply_markup)
-
-                                report.track_screen(user_id,
-                                                    '/%s/%s/%s/%s' % (continent_id, region_id, map_type_id, map_id))
-                                shelve_db.set(chat_id, 'last_map', path)
-                        else:
-                            bot.sendMessage(chat_id, text=info + u'\n<изображение отсутствует>', reply_markup=show_map_reply_markup)
-                            report.track_screen(user_id, '/%s/%s/%s/%s/no_image' % (
-                            continent_id, region_id, map_type_id, map_id))
-
-        shelve_db.close()
-
-    except Exception, err:
-        log.error('start function error: %s' % str(err))
-        log.error(exception_info())
-        report.track_screen(user_id, '/error')
-        bot.sendMessage(chat_id, text=u"Что-то пошло не так. Начать сначала: /start")
 
 
 def legend_handler(bot, update):
@@ -417,31 +291,32 @@ conv_handler = ConversationHandler(
             STATE_TYPE: [MessageHandler([Filters.text], region_handler)],
 
             STATE_MAP: [CommandHandler('start', start),
-                        MessageHandler([Filters.text], type_handler)]
+                        MessageHandler([Filters.text], type_handler),
+                        RegexHandler('^(Обновить)$', refresh_handler),
+                        RegexHandler('^(<<)$', previous_handler),
+                        RegexHandler('^(>>)$', next_handler)]
         },
 
         fallbacks=[CommandHandler('cancel', cancel_handler)]
     )
 
-def error(bot, update, error):
-    log.warn('Update "%s" caused error "%s"' % (update, error))
 
+def error_handler(bot, update, error):
 
-def cb_handler(bot, update):
-    user_id = update.callback_query.from_user.id
-    text = update.callback_query.data
+    user_id = update.message.from_user.id
+    log.error('type_handler error: %s' % str(error))
+    log.error(exception_info())
+    report.track_screen(user_id, '/handle_error')
+
 
 
 updater.dispatcher.add_handler(conv_handler)
-#updater.dispatcher.add_handler(CommandHandler('start', start))
-#updater.dispatcher.add_handler(MessageHandler([Filters.text], start))
 
 updater.dispatcher.add_handler(CommandHandler('legend', legend_handler))
 updater.dispatcher.add_handler(CommandHandler('start', start))
 
-#updater.dispatcher.add_handler(CallbackQueryHandler(cb_handler))
 
-updater.dispatcher.add_error_handler(error)
+updater.dispatcher.add_error_handler(error_handler)
 
 
 from test import test_handler
--- test.py
+++ test.py
@@ -5,15 +5,22 @@ from logger import log
 from telegram.ext import CommandHandler, InlineQueryHandler, CallbackQueryHandler
 from telegram.ext import MessageHandler, Filters
 from telegram import ForceReply, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
-from core import CHANGE_CONT, CHANGE_REGION, CHANGE_MAP, REFRESH, PREVIOUS_MAP, NEXT_MAP
 
 from init import init
-from core import CHANGE_CONT, CHANGE_REGION, CHANGE_MAP, REFRESH, PREVIOUS_MAP, NEXT_MAP
-from core import maps_keyboard_layout, continent_keyboard_layout, region_keyboard_layout
 from core import get_map_id, get_map_type_id, get_region_id, get_continent_id
 from core import get_map, get_continent_name, get_region_name, get_map_type_name
 from core import get_legend, get_previous_timestamp_by_path, get_next_timestamp_by_path
 
+import unittest
+
+
+
+#updater.dispatcher.add_handler(CallbackQueryHandler(cb_handler))
+def cb_handler(bot, update):
+    user_id = update.callback_query.from_user.id
+    text = update.callback_query.data
+
+    
 def test_handler(bot, update):
 
     from datetime import datetime
@@ -47,3 +54,44 @@ def test_handler(bot, update):
     except Exception, err:
         log.error(err)
         bot.sendMessage(chat_id, text=u'<internal error>', reply_markup=reply_markup)
+
+
+class TestBot():
+
+    def __init__(self, user_id):
+        self.user_id = user_id
+        self.sendMessageArgs = {}
+        self.chat_id = None
+
+    def sendMessage(self, chat_id, **kwargs):
+        self.chat_id = chat_id
+        for args in kwargs:
+            self.sendMessageArgs[args] = kwargs[args]
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
+
+
+bot = TestBot('1')
+
+
+class TestStart(unittest.TestCase):
+
+    #self.assertEqual('foo'.upper(), 'FOO')
+
+    #self.assertTrue('FOO'.isupper())
+    #self.assertFalse('Foo'.isupper())
+
+    def test_split(self):
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+        # check that s.split fails when the separator is not a string
+        with self.assertRaises(TypeError):
+            s.split(2)
+
+
+if __name__ == '__main__':
+    unittest.main()
